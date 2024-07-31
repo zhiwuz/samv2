bad8028c8bb131dc038678bc6683012ff07d2959
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-07-31 12:33:58 +0100

    feat: create full pytest marker to download all weights

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,37 @@
+import os
 import subprocess
+from typing import List
 
 import pytest
 
 
 @pytest.fixture
 def download_weights() -> None:
-
-    url: str = (
-        "https://dl.fbaipublicfiles.com/segment_anything_2/072824/sam2_hiera_tiny.pt"
-    )
+    base_url: str = "https://dl.fbaipublicfiles.com/segment_anything_2/072824/"
+    file_names: List[str] = [
+        "sam2_hiera_tiny.pt",
+        "sam2_hiera_small.pt",
+        "sam2_hiera_base_plus.pt",
+        "sam2_hiera_large.pt",
+    ]
     output_directory: str = "artifacts"
 
-    command = ["wget", url, "-P", output_directory]
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
-    try:
-        # Execute the command
-        result = subprocess.run(
-            command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        print("Download completed successfully.")
-        print(result.stdout.decode())
-    except subprocess.CalledProcessError as e:
-        print("An error occurred during the download.")
-        print(e.stderr.decode())
+    for file_name in file_names:
+        file_path = os.path.join(output_directory, file_name)
+        if not os.path.exists(file_path):
+            url = f"{base_url}{file_name}"
+            command = ["wget", url, "-P", output_directory]
+            try:
+                result = subprocess.run(
+                    command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+                print(f"Download of {file_name} completed successfully.")
+                print(result.stdout.decode())
+            except subprocess.CalledProcessError as e:
+                print(f"An error occurred during the download of {file_name}.")
+                print(e.stderr.decode())
+        else:
+            print(f"{file_name} already exists. Skipping download.")
--- tests/test_build_model.py
+++ tests/test_build_model.py
@@ -1,9 +1,15 @@
+import pytest
 import torch
 
 from sam2.build_sam import build_sam2
 
 
-def test_build_sam(download_weights):
+@pytest.mark.full
+@pytest.mark.parametrize(
+    "variant",
+    ["tiny", "small", "base_plus", "large"],
+)
+def test_build_sam(download_weights, variant: str):
     model = build_sam2(
         "sam2_hiera_t.yaml", "./artifacts/sam2_hiera_tiny.pt", device="cpu"
     )
